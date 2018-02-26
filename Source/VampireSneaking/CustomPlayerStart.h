1360ad5320c9dea127d2ec876461be64da31eeab
andesyv <andesyv@gmail.com>
2018-02-26 13:15:39 +0100

    Temp push
    Temporary

---
+++ Source/VampireSneaking/CustomPlayerStart.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "CustomPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAMPIRESNEAKING_API ACustomPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
