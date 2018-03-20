e71699be619e5d86e963ebe6394334ea76875a2c
andesyv <andesyv@gmail.com>
2018-03-20 23:18:24 +0100

    Completed functionality
    The AI now uses a more efficient state switch located in the AI controller.

--- Source/VampireSneaking/AI/EnemyAI.cpp
+++ Source/VampireSneaking/AI/EnemyAI.cpp
@@ -2,7 +2,7 @@
 
 #include "AI/EnemyAI.h"
 #include "Enemy.h"
-#include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
 #include "Engine/TargetPoint.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
@@ -10,6 +10,9 @@
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AIPerceptionTypes.h"
 #include "Perception/AISenseConfig_Sight.h"
+#include "Player/PlayableCharacterBase.h"
+#include "AI/AIVisionCheck.h"
+#include "TimerManager.h"
 
 AEnemyAI::AEnemyAI(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
 
@@ -78,7 +81,7 @@ void AEnemyAI::BeginPlay() {
 	}
 	
 	if (GetPerceptionComp()) {
-		GetPerceptionComp()->OnPerceptionUpdated.AddDynamic(this, &AEnemyAI::UpdatePerception);
+		GetPerceptionComp()->OnPerceptionUpdated.AddDynamic(this, &AEnemyAI::UpdateState);
 	}
 }
 
@@ -96,17 +99,49 @@ void AEnemyAI::UnPossess()
 
 }
 
-void AEnemyAI::UpdatePerception(TArray<AActor*> UpdatedActors) {
-	UE_LOG(LogTemp, Warning, TEXT("Perception updated on!"));
+void AEnemyAI::SetAIIdleState() {
+	if (Blackboard) {
+		if (!Blackboard->SetValue<UBlackboardKeyType_Enum>(TEXT("State"), static_cast<int>(AIState::Idle))) {
+			UE_LOG(LogTemp, Warning, TEXT("Failed to set blackboard state enum."));
+		}
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("Missing blackboard!."));
+	}
+}
 
+void AEnemyAI::UpdateState(TArray<AActor*> UpdatedActors) {
 	for (auto item : UpdatedActors) {
+		if (Cast<APlayableCharacterBase>(item) == nullptr) {
+			continue;
+		}
+
 		FActorPerceptionBlueprintInfo perceivedInfo;
 		if (item && GetPerceptionComp() && GetPerceptionComp()->GetActorsPerception(item, perceivedInfo)) {
 			if (perceivedInfo.LastSensedStimuli.Num() > 0) {
-				if (perceivedInfo.LastSensedStimuli[0].WasSuccessfullySensed()) {
-					UE_LOG(LogTemp, Warning, TEXT("Sensed!"));
+				if (Blackboard) {
+					if (GetWorld() && SearchingTimerHandle.IsValid()) {
+						GetWorld()->GetTimerManager().ClearTimer(SearchingTimerHandle);
+					}
+
+					if (!Blackboard->SetValue<UBlackboardKeyType_Enum>(TEXT("State"), static_cast<int>((perceivedInfo.LastSensedStimuli[0].WasSuccessfullySensed()) ? AIState::Combat : AIState::Searching))) {
+						UE_LOG(LogTemp, Warning, TEXT("Failed to set blackboard state enum."));
+					}
+
+					if (!Blackboard->SetValue<UBlackboardKeyType_Object>(TEXT("TargetActor"), (perceivedInfo.LastSensedStimuli[0].WasSuccessfullySensed()) ? item : nullptr)) {
+						UE_LOG(LogTemp, Warning, TEXT("Failed to set blackboard TargetActor."));
+					}
+
+					if (!perceivedInfo.LastSensedStimuli[0].WasSuccessfullySensed()) {
+						if (!Blackboard->SetValue<UBlackboardKeyType_Vector>(TEXT("LastSeenPosition"), item->GetActorLocation())) {
+							UE_LOG(LogTemp, Warning, TEXT("Failed to set blackboard LastSeenPosition."));
+						}
+
+						if (GetWorld()) {
+							GetWorld()->GetTimerManager().SetTimer(SearchingTimerHandle, this, &AEnemyAI::SetAIIdleState, 3.f);
+						}
+					}
 				} else {
-					UE_LOG(LogTemp, Warning, TEXT("Not sensed!"));
+					UE_LOG(LogTemp, Warning, TEXT("Missing blackboard!."));
 				}
 			}
 		}
