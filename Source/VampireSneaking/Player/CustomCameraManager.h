dc206d678d329b5434f5dc038e3fddf908e99664
andesyv <andesyv@gmail.com>
2018-04-02 21:49:53 +0200

    Added a custom CameraManager

---
+++ Source/VampireSneaking/Player/CustomCameraManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "CustomCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAMPIRESNEAKING_API ACustomCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
